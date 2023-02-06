ffb0709ad914338e9b9ce3b8b3471f5ded2cb0d2
Madhav Goyal <88841339+hydrogenbond007@users.noreply.github.com>
2023-02-06 19:23:10 +0530

    Update hextobase64.rs

--- hextobase64.rs
+++ hextobase64.rs
@@ -1,10 +1,10 @@
-extern crate serialize;
+extern crate rustc_serialize as serialize;
 
-use serialize::base64::{ToBase64, STANDARD};
-use serialize::hex::{FromHex, ToHex};
+use serialize::base64::{self, ToBase64};
+use serialize::hex::FromHex;
 
-fn main () {
-  let stringOfText = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-  let mut config = STANDARD;
-
-  println!("String to base64 = {}", stringOfText.as_bytes().to_base64(config));
+fn main() {
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let result = input.from_hex().unwrap().to_base64(base64::STANDARD);
+    println!("{}", result);
+}
